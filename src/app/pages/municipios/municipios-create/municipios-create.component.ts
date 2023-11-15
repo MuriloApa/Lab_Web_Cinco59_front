bd6e84f83baf23a78ee62e2aad35ecd19f95a887
Murilo Aparecido Carvalho Nascimento <murilo.aparecido@ufms.br>
2023-11-14 23:49:55 -0300

    Término vídeo 2, formulário para cadastro de municipio

--- src/app/pages/municipios/municipios-create/municipios-create.component.ts
+++ src/app/pages/municipios/municipios-create/municipios-create.component.ts
@@ -1,9 +1,12 @@
+import { Municipio } from './../../../models/municipio.model';
 import { MunicipiosService } from './../municipios.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EstadosService } from '../../estados/estados.service';
 import { Estado } from 'src/app/models/estado.model';
 import { CloseScrollStrategy } from '@angular/cdk/overlay';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-municipios-create',
@@ -13,21 +16,59 @@ import { CloseScrollStrategy } from '@angular/cdk/overlay';
 export class MunicipiosCreateComponent implements OnInit {
   estados: Estado[] = [];
 
-  selectedEstado: any;
+  form: FormGroup = new FormGroup({});
 
   constructor(
     private readonly router: Router,
     private readonly municipiosService: MunicipiosService,
     private readonly estadosService: EstadosService,
+    private readonly fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
-    this.estadosService.list().subscribe((resp) => this.estados = resp);
+    this.estadosService.list().subscribe((resp) => {
+      this.estados = resp;
+      this.estados.sort((a: Estado, b: Estado) =>
+        a.sigla.localeCompare(b.sigla)
+      );
+    });
+
+    this.form = this.fb.group({
+      nome: [null, [Validators.required, Validators.minLength(3)]],
+      estado: [null, [Validators.required]],
+      sigla: [null, [ Validators.maxLength(3), Validators.minLength(3)]],
+      ddd: [null, [ Validators.maxLength(2), Validators.minLength(2)]],
+      regiao: [null, [Validators.minLength(3)]],
+    });
   }
 
   save(): void {
-    this.municipiosService.showMessage('Município cadastrado com sucesso!');
-    this.estados.forEach((estado) => console.log(estado))
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      const municipio: Municipio = this.form.value;
+      this.municipiosService
+        .create(municipio)
+        .pipe(
+          catchError((err) => {
+            this.municipiosService.showMessage(
+              'Município não pode ser cadastrado!',
+              true
+            );
+            return err;
+          })
+        )
+        .subscribe((resp) => {
+          this.municipiosService.showMessage(
+            'Município cadastrado com sucesso!'
+          );
+          this.router.navigate(['/municipios']);
+        });
+    } else {
+      this.municipiosService.showMessage(
+        'Existem campos inválidos no formulário!',
+        true
+      );
+    }
   }
 
   cancel(): void {
--- src/app/pages/municipios/municipios.service.ts
+++ src/app/pages/municipios/municipios.service.ts
@@ -1,11 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { Municipio } from 'src/app/models/municipio.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MunicipiosService {
-  constructor(private readonly snackBar: MatSnackBar) {}
+  baseApi: string = '/municipio';
+
+  constructor(
+    private readonly snackBar: MatSnackBar,
+    private readonly http: HttpClient
+  ) {}
+
+  create(municipio: Municipio): Observable<Municipio> {
+    return this.http.post<Municipio>(environment.baseUrl + this.baseApi, municipio);
+  }
 
   showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
@@ -14,6 +27,5 @@ export class MunicipiosService {
       verticalPosition: 'top',
       panelClass: isError ? ['msg-error'] : ['msg-success'],
     });
-
   }
 }
